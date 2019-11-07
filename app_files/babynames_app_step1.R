712c269cb0d00a2b7284492def9344b9f0ec95e9
Lisa Lendway <lisalendway@gmail.com>
2019-11-07 11:22:29 -0600

    list to c()

--- app_files/babynames_app_step1.R
+++ app_files/babynames_app_step1.R
@@ -7,7 +7,7 @@ ui <- fluidPage(
   sliderInput(inputId = "years", label = "Year Range",
               min = 1880, max = 2019, value = c(1880,2019),sep = ""),
   textInput("name", "Name", value = "", placeholder = "Lisa"),
-  selectInput("sex", "Sex", choices = list(Female = "F", Male = "M"))
+  selectInput("sex", "Sex", choices = c(Female = "F", Male = "M"))
 )
 
 server <- function(input, output){}

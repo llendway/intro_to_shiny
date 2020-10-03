aeebef9c21a49cba2285aa23023c4850490ef823
llendway <lisalendway@gmail.com>
2020-10-03 10:52:20 -0500

    tidy babynames_app_basic code

--- app_files/babynames_app_basic.R
+++ app_files/babynames_app_basic.R
@@ -4,10 +4,19 @@ library(tidyverse)
 library(babynames)
 
 ui <- fluidPage(
-  sliderInput(inputId = "years", label = "Year Range",
-              min = 1880, max = 2019, value = c(1880,2019),sep = ""),
-  textInput("name", "Name", value = "", placeholder = "Lisa"),
-  selectInput("sex", "Sex", choices = list(Female = "F", Male = "M")),
+  sliderInput(inputId = "years", 
+              label = "Year Range",
+              min = 1880, 
+              max = 2019, 
+              value = c(1880,2019),
+              sep = ""),
+  textInput("name", 
+            "Name", 
+            value = "", 
+            placeholder = "Lisa"),
+  selectInput("sex", 
+              "Sex", 
+              choices = list(Female = "F", Male = "M")),
   submitButton(text = "Create my plot!"),
   plotOutput(outputId = "timeplot")
 )
@@ -15,7 +24,8 @@ ui <- fluidPage(
 server <- function(input, output) {
   output$timeplot <- renderPlot({
   babynames %>% 
-    filter(name == input$name, sex == input$sex) %>% 
+    filter(name == input$name, 
+           sex == input$sex) %>% 
     ggplot() +
     geom_line(aes(x = year, y = n)) +
     scale_x_continuous(limits = input$years) +

82fb7b288e5e59a667b7abfbc1bae752b197096b
llendway <lisalendway@gmail.com>
2020-10-03 10:49:21 -0500

    tidy up

--- app_files/babynames_app_step1.R
+++ app_files/babynames_app_step1.R
@@ -4,10 +4,19 @@ library(tidyverse)
 library(babynames)
 
 ui <- fluidPage(
-  sliderInput(inputId = "years", label = "Year Range",
-              min = 1880, max = 2019, value = c(1880,2019),sep = ""),
-  textInput("name", "Name", value = "", placeholder = "Lisa"),
-  selectInput("sex", "Sex", choices = c(Female = "F", Male = "M"))
+  sliderInput(inputId = "years", 
+              label = "Year Range",
+              min = 1880, 
+              max = 2019, 
+              value = c(1880,2019),
+              sep = ""),
+  textInput("name", 
+            "Name", 
+            value = "", 
+            placeholder = "Lisa"),
+  selectInput("sex", 
+              "Sex", 
+              choices = c(Female = "F", Male = "M"))
 )
 
 server <- function(input, output){}
